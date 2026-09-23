da89afa2843ee48f02745bda10e5aa6db3c7d7af
1019hao <hyh1019hyh@qq.com>
2026-09-23 22:00:49 +0800

    已由 www.SourceFiles.app 修改

---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import { Store } from './store'
+import Router from './router'
+import Layout from './components/Layout'
+
+function App() {
+  const user = Store.getUser()
+  return user ? <Layout><Router /></Layout> : <Router />
+}
+
+export default App
